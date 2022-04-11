b5990f0ffbf53a1c99a9d1c5af9f6ea221158191
ricardo121990 <ricardo121990@gmail.com>
2022-04-11 14:16:57 -0400

    crontab para verificar servidor

---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"time"
+)
+
+var (
+	serverHost      string
+	serverPort      int
+	checkInterval   int
+	checkTimeout    int
+	printOnlyErrors bool
+	errorLogger     *log.Logger
+	defaultLogger   *log.Logger
+)
+
+func init() {
+	// script arguments
+	flag.StringVar(&serverHost, "host", "192.168.102.214", "Server ip or name to check.")
+	flag.IntVar(&serverPort, "port", 22, "Server TCP port to check.")
+	flag.IntVar(&checkInterval, "interval", 1, "Check interval in seconds.")
+	flag.IntVar(&checkTimeout, "timeout", 60, "Connection timeout in seconds.")
+	flag.BoolVar(&printOnlyErrors, "only_errors", false, "Print only fails.")
+	flag.Parse()
+
+	// loggers
+	errorLogger = log.New(os.Stderr, "ERROR: ", log.LstdFlags)
+	defaultLogger = log.New(os.Stdout, "", log.LstdFlags)
+	if printOnlyErrors {
+		defaultLogger.SetOutput(ioutil.Discard)
+	}
+
+	// check for argument values
+	if serverHost == "" {
+		errorLogger.Fatalln("Server host not set.")
+	}
+
+	if serverPort == 0 {
+		errorLogger.Fatalln("Server port not set.")
+	}
+
+}
+
+func main() {
+	serverAddress := net.JoinHostPort(serverHost, strconv.Itoa(serverPort))
+	timeout := time.Second * time.Duration(checkTimeout)
+
+	defaultLogger.Printf("Starting tcp port check: %s\n", serverAddress)
+	for {
+		tcpConn, tcpErr := net.DialTimeout("tcp", serverAddress, timeout)
+
+		tcpResult := "FAIL"
+		if tcpErr == nil {
+			tcpResult = "OK"
+		}
+
+		if tcpResult == "OK" {
+			tcpConn.Close()
+			defaultLogger.Printf("Connection success to \"%s\"\n", serverAddress)
+		} else {
+			errorLogger.Println("servidor caido")
+		}
+		time.Sleep(time.Second * time.Duration(checkInterval))
+	}
+}
